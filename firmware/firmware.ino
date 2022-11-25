379989ba63e4438d9cda097883de4e21da4a6e04
rameem2003 <rameem2019@gmail.com>
2022-11-25 09:12:09 +0600

    new firmware folder added

---
+++ firmware/firmware.ino
@@ -0,0 +1,17 @@
+/**
+* Arduino Based ROBOT Car Project Firmware Version 1.0 Developer Preview
+* Developer & Contributors
+* REPUBLIC OF LEGENDS
+* No Bluetooth Connectivity
+* ROBOT going forward, backward and stop
+* Added 20/4 LCD display replace 16/2 LCD dispslay
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
